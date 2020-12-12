9c48dbfa8825664a51f26639b89c1f8913ec0afc
Bruno <bruno@Mac-mini-de-Bruno.local>
2020-12-12 19:06:56 -0300

    landing page almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
