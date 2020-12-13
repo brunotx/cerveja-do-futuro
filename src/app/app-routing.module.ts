82d5fe1901ee81fb58543626cada4b815e41798f
Bruno <bruno@Mac-mini-de-Bruno.local>
2020-12-13 09:48:49 -0300

    login and signIn html done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/consumidor/dashboard/dashboard.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 const routes: Routes = [
-  { path: '', component: LandingComponent }
+  { path: '', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signIn', component: SignInComponent},
+  { path: 'dashboard/:id', component: DashboardComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { NavbarComponent } from './pages/navbar/navbar.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { DashboardComponent } from './pages/consumidor/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    SignInComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
